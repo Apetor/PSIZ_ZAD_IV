195c13ef282118655e109b151660029eeb22c257
Apetor <102623708+Apetor@users.noreply.github.com>
2022-06-18 21:11:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,32 @@
-#inclulde <pch.h>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
 
-using namespace std; 
+using namespace std;
+int main(int argc, char** argv) {
+ unique_ptr<logging> log;
+  try {
+    log = make_unique<logging>();
+  } catch (const exception& e) {
+    cout << "Error: logging initialization failed: " << e.what() << endl;
+    return 2;
+  } catch (...) {
+    cout << "Error: logging initialization failed" << endl;
+    return 2;
+  }
+
+  try {
+    if (argc < 3) {
+      cout << "Usage: ber <file 1> <file 2>" << endl;
+      return 1;
+    }
+
+    auto start = chrono::high_resolution_clock::now();
+    log->print("opening file1: ", argv[1]);
+    std::ifstream f1(argv[1]);
+    log->print("opening file2: ", argv[2]);
+    ifstream f2(argv[2]);
+  }
+}
